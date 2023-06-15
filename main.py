b458fbb9b98699cb2a73c98c423241552ca1383e
Afifuddin Mawardi <65497767+afifmawardi@users.noreply.github.com>
2023-06-15 15:20:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+# pip install streamlit yfinance plotly
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from plotly import graph_objs as go
+
+# import library pandas
+import pandas as pd
+
+# Import library numpy
+import numpy as np
+
+# Import library matplotlib untuk visualisasi
+import matplotlib.pyplot as plt
+
+START = "2013-11-19"
+END = "2023-05-19"
+
+st.title('Visualisasi Data Saham Provider Indonesia')
+st.markdown('Visualisasi 4 Perusahaan Provider di Indonesia')
+
+stocks = ('TLKM.JK', 'ISAT.JK', 'EXCL.JK', 'FREN.JK')
+selected_stock = st.selectbox('Pilih dataset ', stocks)
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, END)
+    data.reset_index(inplace=True)
+    return data
+
+if st.button("GO"):	
+	data_load_state = st.text('Loading data...')
+	data = load_data(selected_stock)
+	data_load_state.text('Loading data... done!')
+    # Plot raw data
+	def plot_raw_data():
+		fig = go.Figure()
+		fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="saham"))
+		fig.layout.update(title_text='Saham '+selected_stock, xaxis_rangeslider_visible=True)
+		st.plotly_chart(fig)	
+	plot_raw_data()
+	st.subheader('Historical Data')
+	st.write(data.tail())
+
+st.markdown("""Dibuat Oleh:
+- Afifuddin Mawardi - 1301194113
+- Muhammad Arief  - 1301180265
+- Akhmad Izzaturrafi - 1301180114
+""")
